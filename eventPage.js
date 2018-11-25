4f7da8486c847d2475633cd9f3eb4649b7023c79
Purushottam Sharma <41280402+pm-sharma@users.noreply.github.com>
2018-11-25 15:08:14 +0530

    Upload js and json file

---
+++ eventPage.js
@@ -0,0 +1,25 @@
+var menuItem ={
+	"id": "WS",
+	"title": "WikiPedia Search",
+	"contexts": ["selection"]
+};
+chrome.contextMenus.create(menuItem);
+
+function fixedEncodeURI (str){
+	return encodeURI(str).replace(/%5B/g, '[').replace(/%5D/g, ']');
+}
+
+chrome.contextMenus.onClicked.addListener(function(clickData){
+	if (clickData.menuItemId == "WS" && clickData.selectionText){
+		var wikiUrl ="https://en.wikipedia.org/wiki/" + fixedEncodeURI(clickData.selectionText);
+		var createData = {
+			"url": wikiUrl,
+			"type": "popup",
+			"top": 5,
+			"left": 5,
+			"width": screen.availHeight/2,
+			"height": screen.availHeight/2
+		};
+		chrome.windows.create(createData, function(){});
+	}
+});
